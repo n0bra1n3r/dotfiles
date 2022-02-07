a560314b5f741e73dfe8059920b29323079cce66
Ryan Blonna <n0.b741n37@gmail.com>
2022-02-07 23:27:21 +1100

    Use a different profile for non-projects

--- home/dot_config/nvim/lua/configs/asynctasks.lua
+++ home/dot_config/nvim/lua/configs/asynctasks.lua
@@ -5,7 +5,13 @@ function M.setup()
   vim.g.asynctasks_extra_config = {
     "~/.config/nvim/tasks/global.ini",
   }
-  vim.g.asynctasks_profile = "project"
+
+  if fn.is_git_dir() then
+    vim.g.asynctasks_profile = "project"
+  else
+    vim.g.asynctasks_profile = "file"
+  end
+
   vim.g.asynctasks_template = "~/.config/nvim/tasks/template.ini"
   vim.g.asynctasks_term_focus = 0
   vim.g.asynctasks_term_reuse = 1
