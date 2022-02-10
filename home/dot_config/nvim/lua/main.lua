345aca14dc9ebec8644dca898b8d5a5e85478422
Ryan Blonna <n0.b741n37@gmail.com>
2022-02-10 15:18:07 +1100

    Don't show too many indent markers

--- home/dot_config/nvim/lua/main.lua
+++ home/dot_config/nvim/lua/main.lua
@@ -29,7 +29,7 @@ vim.opt.isfname = "@,48-57,/,\\,.,-,_,+,,,#,$,%,~,="
 vim.opt.isident = "@,48-57,_,192-255"
 vim.opt.linebreak = true
 vim.opt.list = true
-vim.opt.listchars:append("lead:·")
+vim.opt.listchars:append("multispace: ·")
 vim.opt.listchars:append("tab:▸ ")
 vim.opt.listchars:append("trail:•")
 vim.opt.number = true
