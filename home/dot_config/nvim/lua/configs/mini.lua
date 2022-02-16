afcccc07d66d8ffdf6f87b171e140b5f3fd8e15b
Ryan Blonna <n0.b741n37@gmail.com>
2022-02-17 09:58:23 +1100

    Add highlight for word under cursor

--- home/dot_config/nvim/lua/configs/mini.lua
+++ home/dot_config/nvim/lua/configs/mini.lua
@@ -1,6 +1,9 @@
 local M = {}
 
 function M.config()
+  require"mini.cursorword".setup()
+  vim.cmd[[highlight! MiniCursorwordCurrent gui=nocombine guifg=NONE guibg=NONE]]
+
   require"mini.indentscope".setup {
     draw = {
       animation = require"mini.indentscope".gen_animation("none"),
