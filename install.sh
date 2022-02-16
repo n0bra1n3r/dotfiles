49b7532fd59fec573b9cb11383cf06b9db94aca2
Ryan Blonna <n0.b741n37@gmail.com>
2022-02-16 11:51:55 +1100

    Update install script

--- install.sh
+++ install.sh
@@ -2,14 +2,19 @@
 
 set -e
 
+if [ ! "$(command -v git)" ]; then
+  echo "ERROR: git must be installed and in your path" >&2
+  exit 1
+fi
+
+git clone https://github.com/n0bra1n3r/dotfiles.git ~/.local/share/chezmoi
+
 if [ ! "$(command -v chezmoi)" ]; then
-  bin_dir="$HOME/.local/bin"
-  chezmoi="$bin_dir/chezmoi"
-  sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  BIN_DIR="$HOME/.local/bin"
+  chezmoi="$BIN_DIR/chezmoi"
+  sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$BIN_DIR"
 else
   chezmoi=chezmoi
 fi
 
-script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-
-exec "$chezmoi" init --apply "--source=$script_dir"
+exec "$chezmoi" init --apply
