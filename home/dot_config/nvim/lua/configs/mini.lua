3f4df626564674da675e78110607f17e769b6efd
Ryan Blonna <n0.b741n37@gmail.com>
2022-02-17 08:55:03 +1100

    Restore default mappings

--- home/dot_config/nvim/lua/configs/mini.lua
+++ home/dot_config/nvim/lua/configs/mini.lua
@@ -3,14 +3,7 @@ local M = {}
 function M.config()
   require"mini.indentscope".setup {
     draw = {
-      delay = 100,
       animation = require"mini.indentscope".gen_animation("none"),
-    },
-    mappings = {
-      object_scope = '[_',
-      object_scope_with_border = ']|',
-      goto_top = '[-',
-      goto_bottom = ']+',
     },
     symbol = '·',
   }
