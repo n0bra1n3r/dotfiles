41eee5ee4b972054dd6bb380f728e23853c462fa
Ryan Blonna <n0.b741n37@gmail.com>
2022-02-19 02:16:51 +1100

    Remove unneeded autocmd

--- home/dot_config/nvim/lua/configs/vim-floaterm.lua
+++ home/dot_config/nvim/lua/configs/vim-floaterm.lua
@@ -8,7 +8,6 @@ end
 function M.config()
   vim.cmd[[augroup conf_floaterm]]
   vim.cmd[[autocmd!]]
-  vim.cmd[[autocmd FileType floaterm setlocal nonumber]]
   vim.cmd[[autocmd FileType floaterm nnoremap <buffer><silent> <Esc> <cmd>FloatermHide<CR>]]
   vim.cmd[[autocmd VimLeavePre * FloatermKill!]]
   vim.cmd[[augroup end]]
