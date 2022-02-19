5312213b6507c0921feae61fc02e3c6d1340bb0e
Ryan Blonna <n0.b741n37@gmail.com>
2022-02-19 15:31:42 +1100

    Remove highlight customization

--- home/dot_config/nvim/lua/configs/mini.lua
+++ home/dot_config/nvim/lua/configs/mini.lua
@@ -2,7 +2,6 @@ local M = {}
 
 function M.config()
   require"mini.cursorword".setup()
-  vim.cmd[[highlight! MiniCursorwordCurrent gui=nocombine guifg=NONE guibg=NONE]]
 
   require"mini.indentscope".setup {
     draw = {
