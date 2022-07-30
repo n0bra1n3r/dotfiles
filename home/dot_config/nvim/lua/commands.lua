8c62b0d9ff8cbfb4e1a838198e9953f572ed5986
Ryan Blonna <n0.b741n37@gmail.com>
2022-07-31 09:57:37 +1000

    Make module consistent

--- home/dot_config/nvim/lua/commands.lua
+++ home/dot_config/nvim/lua/commands.lua
@@ -1,11 +1,9 @@
-local M = {}
-
-M.G = {
-  function(opts)
-    fn.open_shell("git "..opts.args)
-  end,
-  bang = true,
-  nargs = "+",
+return {
+  G = {
+    function(opts)
+      fn.open_shell("git "..opts.args)
+    end,
+    bang = true,
+    nargs = "+",
+  },
 }
-
-return M
