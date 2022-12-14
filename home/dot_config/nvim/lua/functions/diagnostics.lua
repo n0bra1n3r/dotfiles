8269decc290629de03645cead7ab98598476ebf3
Ryan Blonna <n0.b741n37@gmail.com>
2022-12-14 11:07:09 +1100

    Simplify function

--- home/dot_config/nvim/lua/functions/diagnostics.lua
+++ home/dot_config/nvim/lua/functions/diagnostics.lua
@@ -1,31 +1,7 @@
 local M = {}
 
 function M.get_qf_diagnostic_list()
-  local diagnostics = {}
-  for _, value in ipairs(vim.fn.getqflist()) do
-    local diagnostic = {
-      bufnr = value.bufnr,
-      lnum = value.lnum - 1,
-      end_lnum = value.end_lnum - 1,
-      col = value.col,
-      end_col = value.end_col,
-      message = value.text,
-      source = "quickfix",
-      code = value.nr
-    }
-    if value.type == "E" then
-      diagnostic.severity = vim.diagnostic.severity.ERROR
-    elseif value.type == "N" then
-      diagnostic.severity = vim.diagnostic.severity.HINT
-    elseif value.type == "W" then
-      diagnostic.severity = vim.diagnostic.severity.WARN
-    end
-
-    if diagnostic.severity ~= nil then
-      table.insert(diagnostics, diagnostic)
-    end
-  end
-  return diagnostics
+  return vim.diagnostic.fromqflist(vim.fn.getqflist())
 end
 
 function M.set_qf_diagnostics()
