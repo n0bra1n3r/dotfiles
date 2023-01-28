8c9d80c0ef3dfeb481d6b01b6ea4a693e42d3e8f
Ryan Blonna <n0.b741n37@gmail.com>
2023-01-28 18:28:21 +1100

    Fix quickfix popup

--- home/dot_config/nvim/lua/configs/asyncrun.lua
+++ home/dot_config/nvim/lua/configs/asyncrun.lua
@@ -2,18 +2,25 @@ local M = {}
 
 function M.setup()
   local group = vim.api.nvim_create_augroup("conf_asyncrun", { clear = true })
+
+  local cur_win
+
   vim.api.nvim_create_autocmd("User", {
     pattern = "AsyncRunStart",
     callback = function()
       vim.cmd[[cclose]]
       fn.set_is_job_in_progress(true)
+      cur_win = vim.api.nvim_get_current_win()
     end
   })
   vim.api.nvim_create_autocmd("User", {
     pattern = "AsyncRunStop",
     callback = function()
+      local focus_win = vim.api.nvim_get_current_win()
+      vim.api.nvim_set_current_win(cur_win)
       fn.show_quickfix()
       fn.set_qf_diagnostics()
+      vim.api.nvim_set_current_win(focus_win)
       fn.set_is_job_in_progress(false)
     end
   })
