bf8308e7fafcd693ecfb5921dc8a526be76d02c3
Ryan Blonna <n0.b741n37@gmail.com>
2023-02-09 19:07:00 +1100

    Fix dependency installation script

--- home/.chezmoiscripts/run_after_90-install-deps.sh
+++ home/.chezmoiscripts/run_after_90-install-deps.sh
@@ -2,6 +2,17 @@
 
 set -e
 
-cp -rf ~/.dotfiles/deps/*/.local ~
+shopt -u nullglob
+
+for zip in ~/.dotfiles/deps/*/*.zip; do
+  cd "$(dirname "$zip")" && unzip -oqq "$zip" && rm "$zip"
+  echo "> unzip $zip"
+done
+
+for dir in ~/.dotfiles/deps/*/.local; do
+  cp -rf "$dir" ~
+  echo "> cp -r $dir ~"
+done
 
 chmod 600 ~/.ssh/keys/*
+echo "> chmod 600 ~/.ssh/keys/*"
