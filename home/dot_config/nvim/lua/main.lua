240c3fe56315d93c5d12a104e37feff7a1a0a889
Ryan Blonna <n0.b741n37@gmail.com>
2023-03-02 22:09:33 +1100

    Don't save buffer options in sessions

--- home/dot_config/nvim/lua/main.lua
+++ home/dot_config/nvim/lua/main.lua
@@ -38,7 +38,7 @@ vim.opt.number = true
 vim.opt.numberwidth = 2
 vim.opt.ruler = false
 vim.opt.scrollback = 9001
-vim.opt.sessionoptions = "buffers,curdir,folds,winsize,winpos,localoptions"
+vim.opt.sessionoptions = "buffers,curdir,folds,winsize,winpos"
 vim.opt.shiftwidth = 2
 vim.opt.shortmess:append("sI")
 vim.opt.showbreak = "↪"
