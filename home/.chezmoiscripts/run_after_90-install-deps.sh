921b2da8de3dc7ef1595ce98edf6fdf32b3f956d
Ryan Blonna <n0.b741n37@gmail.com>
2023-03-12 08:11:36 +1100

    Remove automatic download for some dependencies

--- home/.chezmoiscripts/run_after_90-install-deps.sh
+++ home/.chezmoiscripts/run_after_90-install-deps.sh
@@ -5,14 +5,11 @@ set -e
 shopt -u nullglob
 
 for zip in ~/.dotfiles/deps/*/*.zip; do
-  cd "$(dirname "$zip")" && unzip -oqq "$zip" && rm "$zip"
-  echo "> unzip $zip"
+  cd "$(dirname "$zip")" && unzip -oqq "$zip" && rm "$zip" && echo "> unzip $zip"
 done
 
 for dir in ~/.dotfiles/deps/*/.local; do
-  cp -rf "$dir" ~
-  echo "> cp -r $dir ~"
+  cp -rf "$dir" ~ && echo "> cp -r $dir ~"
 done
 
-chmod 600 ~/.ssh/keys/*
-echo "> chmod 600 ~/.ssh/keys/*"
+chmod 600 ~/.ssh/keys/* && echo "> chmod 600 ~/.ssh/keys/*"
