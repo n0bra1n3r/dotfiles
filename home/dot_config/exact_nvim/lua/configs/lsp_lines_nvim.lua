0f215c2043cde46bda1f2395c14ec311601b7b7d
Ryan Blonna <n0.b741n37@gmail.com>
2023-04-24 16:42:23 +1000

    Show diagnostics after delay

--- home/dot_config/exact_nvim/lua/configs/lsp_lines_nvim.lua
+++ home/dot_config/exact_nvim/lua/configs/lsp_lines_nvim.lua
@@ -1,10 +1,45 @@
+local diagnostic_timer
+
+local function disable_diagnostic_display()
+  vim.diagnostic.config{ virtual_lines = false }
+
+  if diagnostic_timer ~= nil then
+    diagnostic_timer:close()
+    diagnostic_timer = nil
+  end
+end
+
+local function display_diagnostics_soon()
+  if diagnostic_timer ~= nil then
+    diagnostic_timer:close()
+    diagnostic_timer = nil
+  end
+
+  diagnostic_timer = vim.loop.new_timer()
+  diagnostic_timer:start(1000, 0, vim.schedule_wrap(function()
+    vim.diagnostic.config{ virtual_lines = { only_current_line = true } }
+    diagnostic_timer:close()
+    diagnostic_timer = nil
+  end))
+end
+
 function plug.config()
   require'lsp_lines'.setup()
 
   vim.diagnostic.config(require'lsp-zero'.defaults.diagnostics {
-    virtual_lines = {
-      only_current_line = true,
-    },
     virtual_text = false,
+  })
+
+  local prev_line
+  vim.api.nvim_create_autocmd("CursorMoved", {
+    group = vim.api.nvim_create_augroup("lsp_lines_conf", { clear = true }),
+    callback = function()
+      local line = vim.api.nvim_get_current_line()
+      if prev_line ~= nil and prev_line ~= line then
+        disable_diagnostic_display()
+        display_diagnostics_soon()
+      end
+      prev_line = line
+    end,
   })
 end
