6d30c7822e6ea7b9e50e9058c963c2acd4c04a7a
Ryan Blonna <n0.b741n37@gmail.com>
2023-04-26 11:01:23 +1000

    Make gutter prettier

--- home/dot_config/exact_nvim/lua/configs/nvim-ufo.lua
+++ home/dot_config/exact_nvim/lua/configs/nvim-ufo.lua
@@ -1,5 +1,6 @@
 function plug.config()
   require'ufo'.setup {
+    open_fold_hl_timeout = 0,
     provider_selector = function()
       return { "treesitter", "indent" }
     end,
---
+++ home/dot_config/exact_nvim/lua/configs/statuscol_nvim.lua
@@ -0,0 +1,19 @@
+function plug.config()
+  require'statuscol'.setup {
+    relculright = true,
+    segments = {
+      {
+        sign = { name = { ".*" }, auto = true },
+        click = "v:lua.ScSa",
+      },
+      {
+        text = { " ", require'statuscol.builtin'.lnumfunc, " " },
+        click = "v:lua.ScLa",
+      },
+      {
+        text = { require'statuscol.builtin'.foldfunc, " " },
+        click = "v:lua.ScFa",
+      },
+    },
+  }
+end
