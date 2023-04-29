9bc1ca4325ff2f95ddfd541e4133cdaa747664c9
Ryan Blonna <n0.b741n37@gmail.com>
2023-04-29 14:35:40 +1000

    Use env var

--- home/dot_config/exact_nvim/lua/configs/asynctasks_vim.lua
+++ home/dot_config/exact_nvim/lua/configs/asynctasks_vim.lua
@@ -1,9 +1,9 @@
 function plug.init()
   vim.g.asynctasks_config_name = ".nvim/tasks.ini"
   vim.g.asynctasks_extra_config = {
-    "~/.config/nvim/tasks/global.ini",
+    vim.env.MYVIMRC.."/tasks/global.ini",
   }
-  vim.g.asynctasks_template = "~/.config/nvim/tasks/template.ini"
+  vim.g.asynctasks_template = vim.env.MYVIMRC.."/tasks/template.ini"
   vim.g.asynctasks_term_focus = 0
   vim.g.asynctasks_term_reuse = 1
 end
