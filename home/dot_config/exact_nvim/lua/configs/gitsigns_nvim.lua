6d098be2954ddfaa672868449de20c92ccb06d3f
Ryan Blonna <n0.b741n37@gmail.com>
2023-05-06 00:34:05 +1000

    Add line blame

--- home/dot_config/exact_nvim/lua/configs/gitsigns_nvim.lua
+++ home/dot_config/exact_nvim/lua/configs/gitsigns_nvim.lua
@@ -1,3 +1,10 @@
 function plug.config()
-  require"gitsigns".setup()
+  require"gitsigns".setup {
+    current_line_blame = true,
+    current_line_blame_formatter = " <author_time:%Y-%m-%d> - <summary>",
+    current_line_blame_opts = {
+      virt_text_pos = "right_align",
+      ignore_whitespace = true,
+    },
+  }
 end
