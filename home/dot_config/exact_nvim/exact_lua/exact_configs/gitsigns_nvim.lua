3e42f81e427f3a3446f30d62055ef3a3036b981b
Ryan Blonna <n0.b741n37@gmail.com>
2023-06-12 00:28:12 +1000

    Right align blame message

--- home/dot_config/exact_nvim/exact_lua/exact_configs/gitsigns_nvim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_configs/gitsigns_nvim.lua
@@ -4,6 +4,7 @@ function plug.config()
     current_line_blame_formatter = " <author_time:%R> - <summary>",
     current_line_blame_opts = {
       ignore_whitespace = true,
+      virt_text_pos = "right_align",
     },
     signs = {
       add = { text = '┃' },
