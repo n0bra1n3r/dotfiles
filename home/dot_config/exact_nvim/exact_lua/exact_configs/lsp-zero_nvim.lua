0fd3450419674e4b67cf7e507ab64a3edbdf6130
Ryan Blonna <n0.b741n37@gmail.com>
2023-06-12 23:02:32 +1000

    Remove excess spacing from diagnostic

--- home/dot_config/exact_nvim/exact_lua/exact_configs/lsp-zero_nvim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_configs/lsp-zero_nvim.lua
@@ -8,7 +8,10 @@ function plug.config()
   vim.diagnostic.config(require'lsp-zero'.defaults.diagnostics {
     severity_sort = true,
     signs = false,
-    virtual_text = { format = function(_) return "" end },
+    virtual_text = {
+      format = function(_) return "" end,
+      spacing = 1,
+    },
   })
 
   lsp.set_sign_icons {
