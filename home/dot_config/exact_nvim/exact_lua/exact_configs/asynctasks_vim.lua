5dd85bbc17f3516e32b328e670cb9c2b18831acf
Ryan Blonna <n0.b741n37+Github@gmail.com>
2023-07-07 17:02:59 +1000

    Fix path for tasks

--- home/dot_config/exact_nvim/exact_lua/exact_configs/asynctasks_vim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_configs/asynctasks_vim.lua
@@ -1,9 +1,11 @@
 function plug.init()
+  local rc_dir = vim.fn.fnamemodify(vim.env.MYVIMRC, ":h")
+
   vim.g.asynctasks_config_name = ".nvim/tasks.ini"
   vim.g.asynctasks_extra_config = {
-    vim.env.MYVIMRC.."/tasks/global.ini",
+    rc_dir.."/tasks/global.ini",
   }
-  vim.g.asynctasks_template = vim.env.MYVIMRC.."/tasks/template.ini"
+  vim.g.asynctasks_template = rc_dir.."/tasks/template.ini"
   vim.g.asynctasks_term_focus = 0
   vim.g.asynctasks_term_reuse = 1
 end
