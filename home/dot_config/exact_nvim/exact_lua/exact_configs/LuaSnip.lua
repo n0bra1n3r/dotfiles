75b9cd470071ec34d2aff40c680dbe9384c890f1
Ryan Blonna <n0.b741n37+Github@gmail.com>
2023-07-11 10:52:29 +1000

    Fix luasnip jump

---
+++ home/dot_config/exact_nvim/exact_lua/exact_configs/LuaSnip.lua
@@ -0,0 +1,15 @@
+function plug.config()
+  vim.api.nvim_create_autocmd("ModeChanged", {
+    group = vim.api.nvim_create_augroup("conf_luasnip", { clear = true }),
+    callback = function()
+      local cur_buf = vim.api.nvim_get_current_buf()
+      local event = vim.v.event
+      if ((event.old_mode == "s" and event.new_mode == "n")
+            or event.old_mode == "i")
+          and require'luasnip'.session.current_nodes[cur_buf]
+          and not require'luasnip'.session.jump_active then
+        require'luasnip'.unlink_current()
+      end
+    end
+  })
+end
