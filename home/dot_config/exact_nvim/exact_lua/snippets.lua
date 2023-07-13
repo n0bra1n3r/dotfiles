490b815a81f010ae98ec0542a847b16b0495b465
Ryan Blonna <n0.b741n37+Github@gmail.com>
2023-07-13 12:42:44 +1000

    Add flutter snippets

--- home/dot_config/exact_nvim/exact_lua/snippets.lua
+++ home/dot_config/exact_nvim/exact_lua/snippets.lua
@@ -1,4 +1,43 @@
 my_snippets {
+  dart = {
+    ["create flutter stateless widget class"] = {
+      prefix = "fstatelesswidget",
+      description = "Create a flutter stateless widget class.",
+      body = [[
+      import 'package:flutter/material.dart';
+
+      class ${TM_FILENAME_BASE/([a-z]*)_*([a-z]*)/${1:/capitalize}${2:/capitalize}/g} extends StatelessWidget {
+        const ${TM_FILENAME_BASE/([a-z]*)_*([a-z]*)/${1:/capitalize}${2:/capitalize}/g}({super.key});
+
+        @override
+        Widget build(BuildContext context) {
+          return ${1:child}(${2:args});
+        }
+      }
+      ]]
+    },
+    ["create flutter stateful widget class"] = {
+      prefix = "fstatefulwidget",
+      description = "Create a flutter stateful widget class.",
+      body = [[
+      import 'package:flutter/material.dart';
+
+      class ${TM_FILENAME_BASE/([a-z]*)_*([a-z]*)/${1:/capitalize}${2:/capitalize}/g} extends StatefulWidget {
+        const ${TM_FILENAME_BASE/([a-z]*)_*([a-z]*)/${1:/capitalize}${2:/capitalize}/g}({super.key});
+
+        @override
+        State<${TM_FILENAME_BASE/([a-z]*)_*([a-z]*)/${1:/capitalize}${2:/capitalize}/g}> createState() => _${TM_FILENAME_BASE/([a-z]*)_*([a-z]*)/${1:/capitalize}${2:/capitalize}/g}State();
+      }
+
+      class _${TM_FILENAME_BASE/([a-z]*)_*([a-z]*)/${1:/capitalize}${2:/capitalize}/g}State extends State<${TM_FILENAME_BASE/([a-z]*)_*([a-z]*)/${1:/capitalize}${2:/capitalize}/g}> {
+        @override
+        Widget build(BuildContext context) {
+          return ${1:child}(${2:args});
+        }
+      }
+      ]]
+    },
+  },
   lua = {
     ["create snippet"] = {
       prefix = "snippet",
