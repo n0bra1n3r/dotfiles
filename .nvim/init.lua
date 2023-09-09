9a269b8cdd2730a622e48e16b6520a5aef4bd3da
Ryan Blonna <n0.b741n37+Github@gmail.com>
2023-09-09 14:19:21 +1000

    Fix nvim task

--- .nvim/init.lua
+++ .nvim/init.lua
@@ -3,6 +3,9 @@ my_autocmds {
     pattern = '*/chezmoi/home/*',
     callback = function(args)
       fn.run_task([[Apply config]], {
+        '--exclude=scripts',
+        '--force',
+        '--source-path',
         vim.fn.fnamemodify(args.file, ':~:.'),
       })
     end,
@@ -14,9 +17,6 @@ my_tasks {
     cmd = 'chezmoi',
     args = {
       'apply',
-      '--exclude=scripts',
-      '--force',
-      '--source-path',
     },
   }
 }
