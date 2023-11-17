056aa12464ceaedc1ae3bd5fc7952b501f320b27
Ryan Blonna <n0.b741n37+Github@gmail.com>
2023-11-17 14:17:05 +1100

    Improve auto-doc prompt

--- home/dot_config/exact_nvim/exact_lua/exact_configs/gp_nvim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_configs/gp_nvim.lua
@@ -7,6 +7,7 @@ return {
             .. "```{{filetype}}\n{{selection}}\n```\n\n"
             .. "Enrich the public API with documentation comments that can be "
             .. "published as formal documentation. Do NOT change the code. "
+            .. "Use end-of-line form for doc comments in dart. "
             .. "Add documentation for parameters, and wrap all comments at 80 "
             .. "columns."
             .. "\n\nRespond only with the snippet of finalized code:"
