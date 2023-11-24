1825eccfba01ec3307719aca295b1696ab357ae7
Ryan Blonna <n0.b741n37+Github@gmail.com>
2023-11-24 21:25:05 +1100

    Switch to wezterm from iterm

---
+++ home/dot_config/exact_wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = 'Catppuccin Frappe'
+config.font = wezterm.font('JetBrainsMonoNL NFP', { weight = 'Medium' })
+config.font_size = 14
+config.initial_cols = 220
+config.initial_rows = 60
+config.scrollback_lines = 9001
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+return config
