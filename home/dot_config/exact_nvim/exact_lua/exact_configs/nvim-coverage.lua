271e2f25d03e3092fd829ffe344e647e71cd777d
Ryan Blonna <n0.b741n37+Github@gmail.com>
2024-01-04 14:45:01 +1100

    Use prettier icon

--- home/dot_config/exact_nvim/exact_lua/exact_configs/nvim-coverage.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_configs/nvim-coverage.lua
@@ -2,7 +2,7 @@ return {
   config = function()
     require'coverage'.setup {
       signs = {
-        covered = { hl = 'CoverageCovered', text = '󰤑 ' },
+        covered = { hl = 'CoverageCovered', text = '󰙨 ' },
         uncovered = { hl = 'CoverageUncovered', text = '󰤑 ' },
         partial = { hl = 'CoveragePartial', text = '󰤑 ' },
       },
