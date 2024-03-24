2b4bcaa3d4c6db1a683aeee21df2380a04d66e95
Ryan Blonna <n0.b741n37@gmail.com>
2024-03-24 11:28:14 +1100

    Fix folds being disabled

--- home/dot_config/exact_nvim/exact_lua/exact_configs/toggleterm_nvim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_configs/toggleterm_nvim.lua
@@ -3,10 +3,6 @@ return {
     require'toggleterm'.setup {
       autochdir = false,
       direction = 'float',
-      on_create = function(t)
-        vim.wo[t.window].foldcolumn = '0'
-        vim.wo[t.window].signcolumn = 'no'
-      end,
       open_mapping = nil,
       shade_terminals = false,
       -- required to avoid '\r\n' for `chansend` in git bash
