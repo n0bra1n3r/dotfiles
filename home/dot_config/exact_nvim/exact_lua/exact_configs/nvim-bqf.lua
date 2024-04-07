3fa5d0db7da278c0aea66dc2545ad6e019759a4d
Ryan Blonna <n0.b741n37@gmail.com>
2024-04-07 21:11:39 +1000

    Make sure bqf is always enabled

--- home/dot_config/exact_nvim/exact_lua/exact_configs/nvim-bqf.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_configs/nvim-bqf.lua
@@ -1,10 +1,13 @@
 return {
   config = function()
     require'bqf'.setup {
+      auto_enable = false,
       enable_mouse = false,
       preview = {
         border = 'single',
       },
     }
+
+    require'bqf'.enable()
   end,
 }
