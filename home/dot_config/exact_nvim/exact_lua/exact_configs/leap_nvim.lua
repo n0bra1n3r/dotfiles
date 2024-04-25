d52979849ac0e38efcec350ff2d6e2d54f086698
Ryan Blonna <n0.b741n37@gmail.com>
2024-04-25 21:58:17 +1000

    Remove leaps usage of special chars

--- home/dot_config/exact_nvim/exact_lua/exact_configs/leap_nvim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_configs/leap_nvim.lua
@@ -1,6 +1,8 @@
 return {
   config = function()
     require'leap'.opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+    require'leap'.opts.labels = 'asdfghjkl;qwertyuiopzxcvbnm'
+    require'leap'.opts.safe_labels = 'sfnut'
     require'leap'.opts.special_keys.prev_target = '<backspace>'
     require'leap'.opts.special_keys.prev_group = '<backspace>'
   end,
