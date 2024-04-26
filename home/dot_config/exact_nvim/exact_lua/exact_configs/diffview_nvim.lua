b40300def1f5824bfff1ace4aa988c670cb8b16e
Ryan Blonna <n0.b741n37@gmail.com>
2024-04-26 12:29:18 +1000

    Set diffview mappings

--- home/dot_config/exact_nvim/exact_lua/exact_configs/diffview_nvim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_configs/diffview_nvim.lua
@@ -1,5 +1,31 @@
 return {
   config = function()
-    require'diffview'.setup{}
+    local actions = require'diffview.actions'
+    require'diffview'.setup {
+      keymaps = {
+        file_panel = {
+          { 'n', [[<Down>]],         actions.select_next_entry },
+          { 'n', [[<Enter>]],        actions.refresh_files },
+          { 'n', [[<LeftMouse>]],    actions.select_entry },
+          { 'n', [[<Up>]],           actions.select_prev_entry },
+          { 'n', [[a]],              actions.toggle_stage_entry },
+          { 'n', [[A]],              actions.stage_all },
+          { 'n', [[j]],              actions.select_next_entry },
+          { 'n', [[k]],              actions.select_prev_entry },
+          { 'n', [[R]],              actions.unstage_all },
+          { 'n', [[r]],              actions.restore_entry },
+          { 'n', [[-]],              false },
+          { 'n', [[<2-LeftMouse>]],  false },
+          { 'n', [[<Tab>]],          false },
+          { 'n', [[<S-Tab>]],        false },
+          { 'n', [[l]],              false },
+          { 'n', [[o]],              false },
+          { 'n', [[S]],              false },
+          { 'n', [[s]],              false },
+          { 'n', [[U]],              false },
+          { 'n', [[X]],              false },
+        },
+      },
+    }
   end
 }
