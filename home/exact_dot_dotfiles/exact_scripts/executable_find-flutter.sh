f3be2a41228e0265efc8f73ec5890c84c22ac20d
Ryan Blonna <n0.b741n37@gmail.com>
2024-07-03 08:10:12 +1000

    Fix flutter dir name

--- home/exact_dot_dotfiles/exact_scripts/executable_find-flutter.sh
+++ home/exact_dot_dotfiles/exact_scripts/executable_find-flutter.sh
@@ -4,5 +4,7 @@ if [[ -f ".puro.json" ]]; then
   PURO_ENV="$(jq -r .env ".puro.json")"
   echo "$HOME/.puro/envs/$PURO_ENV/flutter"
 else
-  type flutter | cut -d' ' -f3-
+  FLUTTER_DIR="$(type flutter | cut -d' ' -f3-)"
+  FLUTTER_DIR="$(dirname "$FLUTTER_DIR")"
+  FLUTTER_DIR="$(dirname "$FLUTTER_DIR")"
 fi
