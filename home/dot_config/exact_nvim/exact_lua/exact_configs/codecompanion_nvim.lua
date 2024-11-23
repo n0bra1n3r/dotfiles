bdd82a389360d7c3be6ed702bf988deb6fddff84
Ryan Blonna <n0.b741n37@gmail.com>
2024-11-23 12:15:09 +1100

    Fix codecompanion config

--- home/dot_config/exact_nvim/exact_lua/exact_configs/codecompanion_nvim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_configs/codecompanion_nvim.lua
@@ -1,6 +1,11 @@
 return {
   config = function()
     require'codecompanion'.setup {
+      display = {
+        chat = {
+          render_headers = false,
+        }
+      },
       strategies = {
         chat = {
           adapter = 'anthropic',
